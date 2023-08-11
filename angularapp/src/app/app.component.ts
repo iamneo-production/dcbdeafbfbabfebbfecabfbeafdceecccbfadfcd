92e46ec4097603dc68f0a9ecaddf9e074d59e210
Iamneo <techadmin@iamneo.ai>
2023-08-11 11:41:42 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularapp';
+  title = 'currency-converter';
+  sourceCurrency: number = 0;
+  targetCurrency: number = 0;
+  amount: number = 0;
+  conversionResult: string = '';
+  resultMessage: string = '';
+
+  performConversion(): void {
+    if (this.sourceCurrency !== 0 && this.targetCurrency !== 0) {
+      this.conversionResult = (Math.round((this.targetCurrency / this.sourceCurrency) * this.amount)).toFixed(2);
+      this.resultMessage = `Converted amount: ${this.conversionResult}`;
+    } else {
+      this.resultMessage = 'Please select valid source and target currencies.';
+    }
+  }
 }
